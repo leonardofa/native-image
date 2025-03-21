93a3ca3b8ffdfc50ec0f0486d6938c003e50716b
Leonardo Ferreira Alves <leonardofa@gmail.com>
2025-03-21 12:16:25 +0000

    Change home page redirection to swagger page

---
+++ springboot-graalvm/src/main/java/br/com/leonardo/estudos/springbootgraalvm/HomeController.java
@@ -0,0 +1,14 @@
+package br.com.leonardo.estudos.springbootgraalvm;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+  @GetMapping("/")
+  public String redirectToSwagger() {
+    return "redirect:/swagger-ui/index.html";
+  }
+
+}
