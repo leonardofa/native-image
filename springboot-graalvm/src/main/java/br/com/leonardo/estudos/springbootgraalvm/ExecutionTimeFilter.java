99a82246694ce9cf8d9f1cfb982f7e374bcf3698
Leonardo Ferreira Alves <leonardofa@gmail.com>
2025-03-28 19:32:39 +0000

    add filter to log requests time

---
+++ springboot-graalvm/src/main/java/br/com/leonardo/estudos/springbootgraalvm/ExecutionTimeFilter.java
@@ -0,0 +1,27 @@
+package br.com.leonardo.estudos.springbootgraalvm;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class ExecutionTimeFilter implements Filter {
+
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+    long startTime = System.currentTimeMillis();
+
+    chain.doFilter(request, response); // Continua com a requisição
+
+    long elapsedTime = System.currentTimeMillis() - startTime;
+    String path = httpServletRequest.getRequestURI();
+    System.out.println("Requisição para [" + path + "] levou " + elapsedTime + " ms.");
+  }
+}
